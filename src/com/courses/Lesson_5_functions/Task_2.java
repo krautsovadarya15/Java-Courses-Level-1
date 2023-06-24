41a6cf7f7d1eb24eb7d427927b14637684a1ffb5
Darya Krautsova <krautsovadarya15@gmail.com>
2023-06-24 15:41:30 +0300

    5.2 Functions

---
+++ src/com/courses/Lesson_5_functions/Task_2.java
@@ -0,0 +1,14 @@
+package com.courses.Lesson_5_functions;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int result2 = getCube(a,b);
+        System.out.println(result2);
+    }
+    public static int getCube(int third, int fourth) {
+        return (int) Math.pow(third, fourth);
+    }
+}
